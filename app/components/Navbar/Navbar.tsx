1cdd8325560bbefe959f000148f2a7d957794417
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2023-10-25 16:55:41 +0000

    Get package version from npm

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,13 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import packageJson from '../../../package.json';
 import GitButton from '../GitButton';
 import styles from './Navbar.module.scss';
 
-const Navbar = () => {
-  let version = packageJson.dependencies.doticons.includes('^')
-    ? packageJson.dependencies.doticons.replace('^', '')
-    : packageJson.dependencies.doticons;
+async function getPackageVersion() {
+  const response = await fetch(`https://registry.npmjs.org/doticons`);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const data = await response.json();
+
+  if (data['dist-tags'] && data['dist-tags'].latest) {
+    return data['dist-tags'].latest;
+  } else {
+    throw new Error(`Package not found on npm.`);
+  }
+}
+
+const Navbar = async () => {
+  let version = 0;
+  await getPackageVersion()
+    .then((v) => (version = v))
+    .catch((error) => console.error(error));
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.nav_grouped}>
