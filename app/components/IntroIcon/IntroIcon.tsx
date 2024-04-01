fbf1a1221959ee3ee771ca83114b46f7f306997e
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-04-01 16:38:30 +0000

    Create intro icon component

---
+++ app/components/IntroIcon/IntroIcon.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { GithubDoticon } from 'doticons/16/index';
+
+const IntroIcon: FC = () => {
+  return <GithubDoticon fill="white" width="567" />;
+};
+
+export default IntroIcon;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import DotsBackground from '@doticons-website/app/components/Background/DotsBackground';
+import IntroIcon from '@doticons-website/app/components/IntroIcon/IntroIcon';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <section className={styles.intro}>
+        <IntroIcon />
+      </section>
       <DotsBackground />
     </main>
   );
