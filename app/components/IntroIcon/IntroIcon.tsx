73312b0ce1defe60a221b6273c8ae0fb63a5d6c9
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-04-04 16:22:21 +0000

    Remove intro icon component

--- app/components/IntroIcon/IntroIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { GithubDoticon } from 'doticons/16/index';
-
-const IntroIcon: FC = () => {
-  return <GithubDoticon fill="white" width="567" />;
-};
-
-export default IntroIcon;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import DotsBackground from '@doticons-website/app/components/Background/DotsBackground';
-import IntroIcon from '@doticons-website/app/components/IntroIcon/IntroIcon';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles.intro}>
-        <IntroIcon />
-      </section>
+      <section className={styles.intro}></section>
       <DotsBackground />
     </main>
   );
