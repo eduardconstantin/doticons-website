cb2f33a5c8abfa5f73dd1455586aab5c0d1ad7f5
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-04-09 17:34:41 +0000

    Add intro section content

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link';
 import DotsBackground from '@doticons-website/app/components/Background/DotsBackground';
 import styles from './page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles.intro}></section>
+      <section className={styles.intro}>
+        <h1>
+          A simple and sleek collection of dot icons crafted in SVG format
+        </h1>
+        <div className={styles.links}>
+          <p>MIT LICENSE</p>
+          <Link href={'#'}>DOCUMENTATION</Link>
+          <Link href={'#'}>NPM PACKAGE</Link>
+        </div>
+      </section>
       <DotsBackground />
     </main>
   );
