6de0661390834ce957290c1c2902a4ddd822dc01
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-04-11 16:57:01 +0000

    Implement search component

---
+++ app/components/Search/Search.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { FC } from 'react';
+import Image from 'next/image';
+import styles from './Search.module.scss';
+
+const Search: FC = () => {
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.search}
+        type="text"
+        placeholder="Search icons"
+        onFocus={(e) => (e.target.placeholder = '')}
+        onBlur={(e) => (e.target.placeholder = 'Search icons')}
+      />
+      <Image
+        draggable="false"
+        width={40}
+        height={40}
+        src="/search32.svg"
+        alt="search icon"
+      />
+    </div>
+  );
+};
+
+export default Search;
