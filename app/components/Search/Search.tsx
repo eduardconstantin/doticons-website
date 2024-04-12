342bcf2a4c4abf837b057f5962479b4d7d77d7eb
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-04-12 16:38:17 +0000

    Implement scroll to section

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -11,6 +11,16 @@ const Search: FC = () => {
         className={styles.search}
         type="text"
         placeholder="Search icons"
+        onClick={() => {
+          const element = document.getElementById('content');
+          const elementPosition = element!.getBoundingClientRect().top;
+          const offsetPosition = elementPosition - 182;
+
+          window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth',
+          });
+        }}
         onFocus={(e) => (e.target.placeholder = '')}
         onBlur={(e) => (e.target.placeholder = 'Search icons')}
       />
--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,25 @@ import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <section className={styles.intro}>
-        <h1>
-          A simple and sleek collection of dot icons crafted in SVG format
-        </h1>
-        <div className={styles.links}>
-          <p>MIT LICENSE</p>
-          <Link href="https://github.com/eduardconstantin/doticons">
-            DOCUMENTATION
-          </Link>
-          <Link href="https://www.npmjs.com/package/doticons">NPM PACKAGE</Link>
+        <div className={styles.introContent}>
+          <h1>
+            A simple and sleek collection of dot icons crafted in SVG format
+          </h1>
+          <div className={styles.links}>
+            <p>MIT LICENSE</p>
+            <Link href="https://github.com/eduardconstantin/doticons">
+              DOCUMENTATION
+            </Link>
+            <Link href="https://www.npmjs.com/package/doticons">
+              NPM PACKAGE
+            </Link>
+          </div>
         </div>
       </section>
       <Search />
+      <section id="content" className={styles.content}></section>
       <DotsBackground />
     </main>
   );
