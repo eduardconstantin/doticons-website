84754df151a92b8a6cf90d2f121f9d0f2360ffc7
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-05-31 16:53:52 +0000

    Add random icon selection

--- app/components/Background/DotsBackground.tsx
+++ app/components/Background/DotsBackground.tsx
@@ -1,13 +1,34 @@
 'use client';
 
-import { type FC } from 'react';
+import {
+  useEffect,
+  useState,
+  type FC,
+  type ForwardRefExoticComponent,
+  type SVGProps,
+} from 'react';
 import useDots from '@doticons-website/app/hooks/useDots';
 import * as Icons16 from 'doticons/16/index';
 import styles from './DotsBackground.module.scss';
 
 const DotsBackground: FC = () => {
   const { cellSize, dotRadius, iconPos, viewBox } = useDots();
-  const { GithubDoticon } = Icons16;
+  const [Icon, setIcon] = useState<
+    ForwardRefExoticComponent<SVGProps<SVGSVGElement>>
+  >(
+    Icons16['GithubDoticon'] as ForwardRefExoticComponent<
+      SVGProps<SVGSVGElement>
+    >
+  );
+
+  useEffect(() => {
+    const randomNo = Math.floor(Math.random() * Object.keys(Icons16).length);
+    setIcon(
+      Icons16[
+        Object.keys(Icons16)[randomNo] as keyof typeof Icons16
+      ] as ForwardRefExoticComponent<SVGProps<SVGSVGElement>>
+    );
+  }, []);
 
   return (
     <div className={styles.background}>
@@ -27,12 +48,7 @@ const DotsBackground: FC = () => {
               fillOpacity="0.1"
             />
           </pattern>
-          <GithubDoticon
-            viewBox={undefined}
-            width="100%"
-            height="100%"
-            id="doticon"
-          />
+          <Icon viewBox={undefined} width="100%" height="100%" id="doticon" />
         </defs>
 
         <rect width="100%" height="100%" fill="url(#dotPattern)" />
