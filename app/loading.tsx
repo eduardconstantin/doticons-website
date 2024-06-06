c0007fc5184ca57a17f38d65a93ff80877362bc4
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-06-06 17:55:02 +0000

    Add logo to loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function Loading() {
-  return <div className="loading">Loading icons</div>;
+  return (
+    <div className="loading">
+      <Image
+        draggable="false"
+        width={200}
+        height={70}
+        src="/DotIcons.svg"
+        alt="Doticons Logo"
+      />
+    </div>
+  );
 }
