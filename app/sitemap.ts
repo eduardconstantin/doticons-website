c65c25f6fd0d02dbb8dfee4c5380da3ea94cb182
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2025-10-24 16:56:47 +0000

    Add static sitemap file to root

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://doticons-website.vercel.app/',
+      lastModified: new Date('2024-07-22T16:05:15+00:00'),
+    },
+  ];
+}
